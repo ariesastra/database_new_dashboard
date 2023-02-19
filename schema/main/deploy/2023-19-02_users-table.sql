f54246c360d84d99163762f3ca57dac64b86ad6e
Arie Sastra <ariesastra@Aries-MacBook-Pro.local>
2023-02-19 14:00:49 +0700

    add users table

---
+++ schema/main/deploy/2023-19-02_users-table.sql
@@ -0,0 +1,18 @@
+-- Deploy dashboard_ads:2023-19-02_users-table to pg
+
+BEGIN;
+
+CREATE TABLE main.users (
+  id                  TEXT NOT NULL,
+  email               TEXT NOT NULL,
+  full_name           TEXT DEFAULT NULL,
+  gender              TEXT DEFAULT NULL,
+  access              TEXT NOT NULL,
+  created_at          TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_by          TEXT NOT NULL DEFAULT 'system',
+  last_updated_at     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  last_updated_by     TEXT DEFAULT 'system',
+  PRIMARY KEY(id)
+);
+
+COMMIT;
