0d8476efdb94f279602f24122606f25b3871771a
Arie Sastra <ariesastra@192.168.1.2>
2023-02-28 20:05:45 +0700

    init auth token table

---
+++ schema/main/deploy/2023-28-02_auth-token.sql
@@ -0,0 +1,7 @@
+-- Deploy dashboard_ads:2023-28-02_auth-token to pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ schema/main/revert/2023-28-02_auth-token.sql
@@ -0,0 +1,7 @@
+-- Revert dashboard_ads:2023-28-02_auth-token from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
