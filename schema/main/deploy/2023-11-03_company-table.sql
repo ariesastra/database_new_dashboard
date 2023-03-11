33550c8795c3117a8aa769c89515d394ccdd818d
Arie Sastra <ariesastra@Aries-MacBook-Pro.local>
2023-03-11 21:26:56 +0700

    change naming

---
+++ schema/main/deploy/2023-11-03_company-table.sql
@@ -0,0 +1,7 @@
+-- Deploy dashboard_ads:2023-11-03_company-table to pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ schema/main/revert/2023-03-11_company-table.sql
@@ -0,0 +1,7 @@
+-- Revert dashboard_ads:2023-11-03_company-table from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
