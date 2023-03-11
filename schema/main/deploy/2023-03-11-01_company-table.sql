077043ea8045ffdd54db8f8fd9c61460304a6a81
Arie Sastra <ariesastra@Aries-MacBook-Pro.local>
2023-03-11 21:40:13 +0700

    add table company, campaign, and platform

---
+++ schema/main/deploy/2023-03-11-01_company-table.sql
@@ -0,0 +1,14 @@
+-- Deploy dashboard_ads:2023-11-03_company-table to pg
+
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS main.company (
+  id            TEXT NOT NULL PRIMARY KEY,
+  user_id       TEXT NOT NULL,
+  company_name  TEXT NOT NULL,
+  company_img   JSONB,
+);
+
+CREATE INDEX IF NOT EXISTS idx_company_name ON main.company (company_name);
+
+COMMIT;
--- schema/main/deploy/2023-11-03_company-table.sql
+++
@@ -1,7 +0,0 @@
--- Deploy dashboard_ads:2023-11-03_company-table to pg
-
-BEGIN;
-
--- XXX Add DDLs here.
-
-COMMIT;
