c81b676c6a3fd19fb8510151cc67e386c1e280e4
Arie Sastra <ariesastra@Aries-MacBook-Pro.local>
2023-03-12 14:25:39 +0700

    add audit column

--- schema/main/deploy/2023-03-11-03_campaign-table.sql
+++ schema/main/deploy/2023-03-11-03_campaign-table.sql
@@ -6,7 +6,11 @@ CREATE TABLE IF NOT EXISTS main.campaign (
   id            TEXT NOT NULL PRIMARY KEY,
   company_id    TEXT NOT NULL,
   platform_id   TEXT NOT NULL,
-  campaign_name TEXT NOT NULL
+  campaign_name TEXT NOT NULL,
+  created_at        TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_by        TEXT NOT NULL DEFAULT 'system',
+  last_updated_at   TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  last_updated_by   TEXT DEFAULT 'system'
 );
 
 CREATE INDEX IF NOT EXISTS idx_campaign ON main.campaign (campaign_name);
